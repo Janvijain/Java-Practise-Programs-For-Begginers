010d8714542e15b8844116afd42ffc3f95bb63fc
Janvi Shree Shrimal <59120284+Janvijain@users.noreply.github.com>
2021-12-06 11:22:44 +0530

    Add files via upload

---
+++ Number of Vowels & Consonants/vowelsconsonant.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+class MyClass
+{
+  public static void main(String[] args)
+  {
+    String str;
+    Scanner a = new Scanner(System.in);
+    str = a.nextLine();
+    int vowels=0;
+    int consonant=0;
+    int i;
+    for(i=0;i<str.length();i++)
+    {
+      char ch = str.charAt(i);
+                //ch = Character.toLowerCase(ch);
+     
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+                {
+                  vowels++;
+                }
+      			else if ( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') )
+                {
+        			consonant++;
+                }
+    			else
+                {
+                  i++;
+                }
+    }
+    System.out.println("Number of Vowels: "+ vowels);
+    System.out.println("Number of Consonants: "+ consonant);
+  }
+}
