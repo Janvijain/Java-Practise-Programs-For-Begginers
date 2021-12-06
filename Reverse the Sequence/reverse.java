6e29ddc52a8445b64373f803341c64ffe0b66982
Janvi Shree Shrimal <59120284+Janvijain@users.noreply.github.com>
2021-12-06 11:13:55 +0530

    Add files via upload

---
+++ Reverse the Sequence/reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class MyClass
+{
+  public static void main(String[]args)
+  {
+    int num=0;
+    int reversenum=0;
+    Scanner in = new Scanner(System.in);
+    num=in.nextInt();
+    while(num!=0)
+    {
+      reversenum=reversenum*10;
+      reversenum=reversenum + num%10;
+      num=num/10;
+    }
+    System.out.println("Reverse of the number is "+reversenum);
+  }
+}
